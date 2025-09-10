6898bea089b6b7037a77b6693adc03279c7f604c
Ming <mxu154@gmail.com>
2025-09-10 19:12:29 +0800

    Update get_plugin_info.sh

--- .github/scripts/get_plugin_info.sh
+++ .github/scripts/get_plugin_info.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-# 获取插件名称和版本
+# 获取插件名称和版本读取Makefile文件
+MAKEFILE="./package/openclash-guard/Makefile"
 
-# 读取Makefile文件
-MAKEFILE="./Makefile"
+PKG_NAME=$(grep -E '^PKG_NAME:=' $MAKEFILE | cut -d '=' -f 2 | tr -d ' ')
+PKG_VERSION=$(grep -E '^PKG_VERSION:=' $MAKEFILE | cut -d '=' -f 2 | tr -d ' ')
 
-# 提取NAME, PKG_NAME和PKG_VERSION
-NAME=$(grep -E '^NAME:=' $MAKEFILE | cut -d '=' -f 2)
-PKG_NAME=$(grep -E '^PKG_NAME:=' $MAKEFILE | cut -d '=' -f 2)
-PKG_VERSION=$(grep -E '^PKG_VERSION:=' $MAKEFILE | cut -d '=' -f 2)
-echo "插件名称：$NAME"
 echo "插件包名称：$PKG_NAME"
 echo "插件版本：$PKG_VERSION"
 
-# 替换PKG_NAME中的$(NAME)
-PLUGIN_NAME=${PKG_NAME//\$(NAME)/$NAME}
-echo "插件合并名称：$PLUGIN_NAME"
-
-# 输出到$GITHUB_ENV
-echo "PLUGIN_NAME=$PLUGIN_NAME" >> $GITHUB_ENV
+echo "PLUGIN_NAME=$PKG_NAME" >> $GITHUB_ENV
 echo "PLUGIN_VERSION=$PKG_VERSION" >> $GITHUB_ENV
